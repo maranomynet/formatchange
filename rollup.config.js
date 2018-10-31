98a19571c4960027810bf6c6f9fcc3c6b9b5716e
mar <mar.nospam@anomy.net>
2018-10-31 14:51:24 +0000

    chore: Add automatic minification for formatchange-min.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,25 @@
 import buble from 'rollup-plugin-buble';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
-    input: 'src/react.js',
-    plugins: [buble({ objectAssign: true })],
-    output: {
-        strict: false,
-        interop: false,
-        file: 'react.js',
-        format: 'cjs',
+export default [
+    {
+        input: 'src/react.js',
+        plugins: [buble({ objectAssign: true })],
+        output: {
+            strict: false,
+            interop: false,
+            file: 'react.js',
+            format: 'cjs',
+        },
     },
-};
+    {
+        input: 'formatchange.js',
+        plugins: [uglify({ sourcemap:true, output:{ comments:'some' } })],
+        output: {
+            strict: false,
+            interop: false,
+            file: 'formatchange-min.js',
+            format: 'cjs',
+        },
+    },
+];
